ba383a7e302b4761053bdeae388d04b279bed0f6
Guilherme Prata Costa <guilherme.costa@fcamara.com.br>
2020-01-21 19:45:53 -0300

    Refatorando codigo e criando widget de Formulariotransferencia

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(ByteBankApp());
+
+class ByteBankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       //Home: Permite indicar qualquer widget como primeira tela. no root do MaterialApp
       home: Scaffold(
         // Scaffold: Permitirá implementarmos toda a estrutura básica (ou esqueleto) do Material Design.
 
-        body: ListaTransferencia(),
-
-        //Navigation Bar no topo
-        appBar: AppBar(
-          title: Text('Transferências ',textDirection: TextDirection.ltr),
-        ),
-
-        floatingActionButton: FloatingActionButton(
-
-          child: Icon(Icons.add),
-        ),
+        //Corpo do aplicativo
+        body: FormularioTransferencia(),
       ),
-    )
-);
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Criando transferência'),
+      ),
+
+      body: Text("Body"),
+    );
+  }
+}
 
 class ListaTransferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ItemTransferencia(Transferencia(100.0, 1000)),
-        ItemTransferencia(Transferencia(200.0, 1001)),
-        ItemTransferencia(Transferencia(300.0, 1002)),
-      ],
+    return Scaffold(
+      //Navigation Bar no topo
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(100.0, 1000)),
+          ItemTransferencia(Transferencia(200.0, 1001)),
+          ItemTransferencia(Transferencia(300.0, 1002)),
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
 
 class ItemTransferencia extends StatelessWidget {
-
   final Transferencia _transferencia;
 
   ItemTransferencia(this._transferencia);
@@ -46,15 +66,14 @@ class ItemTransferencia extends StatelessWidget {
           leading: Icon(Icons.monetization_on),
           title: Text(_transferencia.valor.toString()),
           subtitle: Text(_transferencia.numeroConta.toString()),
-    )
+        )
     );
   }
 }
 
-
-class Transferencia{
+class Transferencia {
   final double valor;
   final int numeroConta;
 
-  Transferencia(this.valor,this.numeroConta);
+  Transferencia(this.valor, this.numeroConta);
 }
