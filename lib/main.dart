496babeb259606b923e809a251ff7c222d9e1a02
Guilherme <guilherme.costa@fcamara.com.br>
2020-01-27 12:19:45 -0300

    Mudando as cores

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,10 @@ class ByteBankApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primaryColor: Colors.green[900],
-        accentColor: Colors.blueAccent[700],
+        primaryColor: Colors.purple[700],
+        accentColor: Colors.deepPurpleAccent[700],
         buttonTheme: ButtonThemeData(
-          buttonColor:  Colors.blueAccent[700],
+          buttonColor:  Colors.deepPurpleAccent[700],
           textTheme: ButtonTextTheme.primary
         )
       ),
