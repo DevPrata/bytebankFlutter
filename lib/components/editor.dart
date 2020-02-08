0818914a434fdbc60e90c0024fd97295fb6e06b9
Guilherme <guilherme.costa@fcamara.com.br>
2020-02-08 13:33:48 -0300

    [Componente] Inserindo props de padding e tipo de teclado no editor

--- lib/components/editor.dart
+++ lib/components/editor.dart
@@ -6,27 +6,32 @@ class Editor extends StatelessWidget {
   final String placeholder;
   final String dica;
   final IconData icone;
+  final EdgeInsets padding;
+  final TextInputType tipoTeclado;
+  //TODO: Refazer logica do tipo de teclado para pegar como defeault texto, rever a tela de formulario em transferencia
 
   Editor({
     this.controlador,
     this.placeholder,
     this.dica,
-    this.icone
+    this.icone,
+    this.padding,
+    this.tipoTeclado
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(40.0, 18.0, 40.0, 12.0),
+      padding: padding != null ? padding : const EdgeInsets.fromLTRB(40.0, 18.0, 40.0, 12.0),
 
       child: TextField(
         controller: controlador,
         style: TextStyle(fontSize: 24.0),
-        keyboardType: TextInputType.number,
+        keyboardType: tipoTeclado != null ? tipoTeclado : TextInputType.number,
         decoration: InputDecoration(
             icon: icone != null ? Icon(icone) : null,
             labelText: placeholder,
-            hintText: dica
+            hintText: dica != null ? dica : null
         ),
       ),
 
